a0ee1ab781153f11acc090a098a1ae8ff7ec90e6
Thomas Ackermann <th.acker@arcor.de>
2023-08-27 17:13:17 +0200

    Pico_LCD_114_V2.py: add memory saving mode and consts for all Pins

--- Pico_LCD_114_V2.py
+++ Pico_LCD_114_V2.py
@@ -1,8 +1,12 @@
 
 #
-# slightly enhanced python driver from
-# https://www.waveshare.com/wiki/Pico-LCD-1.14
-# runs with standard MicroPython 1.19 or later
+#  based on https://www.waveshare.com/wiki/Pico-LCD-1.14
+#
+#  - runs with standard MicroPython 1.19 or later
+#  - can be configured to use only part of the physical display to
+#    save FrameBuffer memory if needed; the remaining vertical resp.
+#    horizontal borders can be colored individually
+#  - additional color definitions
 #
 
 from machine import Pin, SPI, PWM
@@ -10,38 +14,61 @@ import framebuf
 import time
 
 class LCD_114(framebuf.FrameBuffer):
-
-    BL  = 13
+    
+    # Display size
+    WIDTH  = 240
+    HEIGHT = 135
+    # Keys
+    KEY_A = 15
+    KEY_B = 17
+    KEY_2 = 2
+    KEY_3 = 3
+    KEY_4 = 16
+    KEY_5 = 18
+    KEY_6 = 20
+    # Control lines
     DC  = 8
-    RST = 12
-    MOSI= 11
-    SCK = 10
     CS  = 9
-
-    def __init__(self):
-        self.width = 240
-        self.height = 135
-
+    RST = 12
+    # SPI interface
+    SCK = 10
+    MOSI= 11
+    # PWM line
+    BL  = 13
+    
+    def __init__(self, width=WIDTH, height=HEIGHT, v_border_color=0x0000, h_border_color=0x0000):
+        if width < 0:
+            width = 0
+        if width > LCD_114.WIDTH:
+            width = LCD_114.WIDTH
+        if height < 0:
+            height = 0
+        if height > LCD_114.HEIGHT:
+            height = LCD_114.HEIGHT
+        self.width  = width
+        self.height = height
         self.spi = SPI(1, 10_000_000, sck=Pin(LCD_114.SCK), mosi=Pin(LCD_114.MOSI))
         self.pwm = PWM(Pin(LCD_114.BL))
         self.pwm.freq(100)
         self.pwm.duty_u16(32768)
-
         self.rst = Pin(LCD_114.RST, Pin.OUT)
         self.cs  = Pin(LCD_114.CS,  Pin.OUT)
         self.cs(1)
         self.dc  = Pin(LCD_114.DC,  Pin.OUT)
         self.dc(1)
-        self.keyA = Pin(15, Pin.IN, Pin.PULL_UP)
-        self.keyB = Pin(17, Pin.IN, Pin.PULL_UP)
-        self.key2 = Pin(2,  Pin.IN, Pin.PULL_UP)
-        self.key3 = Pin(3,  Pin.IN, Pin.PULL_UP)
-        self.key4 = Pin(16, Pin.IN, Pin.PULL_UP)
-        self.key5 = Pin(18, Pin.IN, Pin.PULL_UP)
-        self.key6 = Pin(20, Pin.IN, Pin.PULL_UP)
-
+        self.keyA = Pin(LCD_114.KEY_A, Pin.IN, Pin.PULL_UP)
+        self.keyB = Pin(LCD_114.KEY_B, Pin.IN, Pin.PULL_UP)
+        self.key2 = Pin(LCD_114.KEY_2,  Pin.IN, Pin.PULL_UP)
+        self.key3 = Pin(LCD_114.KEY_3,  Pin.IN, Pin.PULL_UP)
+        self.key4 = Pin(LCD_114.KEY_4, Pin.IN, Pin.PULL_UP)
+        self.key5 = Pin(LCD_114.KEY_5, Pin.IN, Pin.PULL_UP)
+        self.key6 = Pin(LCD_114.KEY_6, Pin.IN, Pin.PULL_UP)
         self.buffer = bytearray(self.height * self.width * 2)
         super().__init__(self.buffer, self.width, self.height, framebuf.RGB565)
+        # vertical and horizontal borders
+        self.border_buffer = bytearray(2)
+        self.v_border_color = int(v_border_color)
+        self.h_border_color = int(h_border_color)
         # RGB565 colors (see https://rgbcolorpicker.com/565; LSB first!)
         self.white  = 0xffff
         self.black  = 0x0000
@@ -145,15 +172,49 @@ class LCD_114(framebuf.FrameBuffer):
         self.cs(1)
         self.dc(1)
         self.cs(0)
-        self.spi.write(self.buffer)
+        ptr = 0
+        self.border_buffer[0] = self.v_border_color % 256
+        self.border_buffer[1] = self.v_border_color // 256
+        for line in range(0, self.height):
+            self.spi.write(self.buffer[ptr:ptr+2*self.width])
+            ptr = ptr + 2*self.width
+            for i in range(0, (LCD_114.WIDTH-self.width)*2, 2):
+                self.spi.write(self.border_buffer)
+        self.border_buffer[0] = self.h_border_color % 256
+        self.border_buffer[1] = self.h_border_color // 256
+        for line in range(0, LCD_114.HEIGHT-self.height):
+            for i in range(0, LCD_114.WIDTH*2, 2):
+                self.spi.write(self.border_buffer)
         self.cs(1)
-  
+
+    def display(self, pos, label, msg, warning=False, error=False):
+        pos = (pos % 6) * 2
+        w = 220
+        h = 14
+        x = 14
+        y = 14 + pos * (h + 1)
+        fg = self.green
+        bg = self.black
+        if warning:
+            fg = self.yellow
+        if error:
+            fg = self.red
+        self.rect(x, y, w, 2*(h+1), bg, True)
+        self.text(label, x, y, fg)
+        y = y + h
+        self.text(msg, x, y, fg)
+        self.show()
+        
 if __name__=='__main__':
 
-    LCD = LCD_114()
+    LCD = LCD_114(width=230, height=125, v_border_color=0xe007, h_border_color=0x1f00)
     LCD.fill(LCD.black)
     LCD.show()
-
+    
+    import gc
+    gc.collect()
+    print(gc.mem_free())
+    
     LCD.text("red", 90, 20, LCD.red)
     LCD.text("green", 90, 40, LCD.green)
     LCD.text("yellow", 90, 60, LCD.yellow)
@@ -170,12 +231,16 @@ if __name__=='__main__':
         
         if LCD.keyA.value() == 0:
             LCD.fill_rect(208, 12, 20, 20, LCD.red)
+            LCD.v_border_color = LCD.red
+            LCD.h_border_color = LCD.yellow
         else:
             LCD.fill_rect(208, 12, 20, 20, LCD.white)
             LCD.rect(208, 12, 20, 20, LCD.red)
 
         if LCD.keyB.value() == 0:
             LCD.fill_rect(208, 103, 20, 20, LCD.red)
+            LCD.v_border_color = LCD.green
+            LCD.h_border_color = LCD.blue
         else:
             LCD.fill_rect(208, 103, 20, 20, LCD.white)
             LCD.rect(208, 103, 20, 20, LCD.red)
