369dd8814e07c0fb7d9225bb05d267d4cf1e9bd9
Thomas Ackermann <th.acker@arcor.de>
2023-11-22 15:32:52 +0000

    blescan.py: Discover Services and Characteristics

--- blescan.py
+++ blescan.py
@@ -2,7 +2,7 @@
 # https://github.com/micropython/micropython-lib/tree/master/micropython/bluetooth/aioble
 
 import gc
-import uasyncio as asyncio
+import asyncio
 import aioble
 
 devices = dict()
@@ -12,6 +12,7 @@ async def scan():
             device = result.device.addr_hex()
             if device not in devices:
                 device_data = dict()
+                device_data["device"]          = result.device
                 device_data["manufacturer_id"] = 0
                 device_data["manufacturer"]    = set()
                 device_data["services"]        = set()
@@ -35,22 +36,50 @@ def hexdump(bytesset):
         for b in bs:
             bs_hex += f"{b:02x}" + " "
             bs_str += (chr(b) if 32 <= b <= 127 else ".") + " "
-        print(bs_hex, bs_str)
-        
+        print(" ", bs_hex, bs_str)
+
+async def discover(device):
+    conn = None
+    try:
+        conn = await device.connect()
+    except:
+        pass
+    servs = list()
+    if conn != None and conn._conn_handle > 0:
+        async for serv in conn.services():
+            servs.append(serv)
+        for serv in servs:
+            print(" ", serv)
+            async for char in serv.characteristics():
+                print("   ", char)
+    if conn != None:
+        await conn.disconnect()
+    if len(servs) == 0:
+        print("  -")
+    
 async def main():
     while True:
-        print("=== scanning BLE devices ... ===")
+        print("=== Scanning BLE Devices ... ===")
         await scan()
         for device in sorted(devices):
             print()
-            print(device, devices[device]["name"], hex(devices[device]["manufacturer_id"]), devices[device]["rssi"], devices[device]["connectable"])
-            print("manufacturer data")
+            print(devices[device]["device"], devices[device]["name"], hex(devices[device]["manufacturer_id"]), devices[device]["rssi"], devices[device]["connectable"])
+            print("Manufacturer Data")
             hexdump(devices[device]["manufacturer"])
-            print("services")
+            if len(devices[device]["manufacturer"]) == 0:
+                print("  -")
+            print("Advertised Services")
             for service in devices[device]["services"]:
-                print(service)
+                print(" ", service)
+            if len(devices[device]["services"]) == 0:
+                print("  -")
+            print("Services")
+            if devices[device]["connectable"]:
+                await discover(devices[device]["device"])
+            else:
+                print("  -")
         gc.collect()
-        print("===", len(devices), "devices found;", gc.mem_free(), "free mem ===")
+        print("\n===", len(devices), "Devices found;", int(gc.mem_free()/1024), "kB mem free ===\n")
         await asyncio.sleep_ms(10000)
         
 asyncio.run(main())
